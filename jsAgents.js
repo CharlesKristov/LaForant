b054f85d8cc15d1df723ac3ac398b765e8375bea
randyyz <72442728+randyyz@users.noreply.github.com>
2021-05-16 12:21:46 +0700

    added ability desc

--- jsAgents.js
+++ jsAgents.js
@@ -18,4 +18,51 @@ $(document).ready(function(){
             prevImg.addClass('active').css('z-index', 10);
         }
     });
+
+    var delet = $('.yes');
+    var overlay1 = $('.overlay1');
+    var overlay2 = $('.overlay2');
+    var overlay3 = $('.overlay3');
+    var overlay4 = $('.overlay4');
+    $('.card1').hover(function(){
+        delet.addClass('yes').css('opacity', 0);
+        delet.addClass('yes').css('transition', "0.5s");
+        overlay1.addClass('overlay1').css('opacity', 1);
+        
+    }, function(){
+        delet.addClass('yes').css('opacity', "100%");
+        overlay1.addClass('overlay1').css('opacity', -1);
+    });
+
+    $('.card2').hover(function(){
+        delet.addClass('yes').css('opacity', 0);
+        delet.addClass('yes').css('transition', "0.5s");
+        overlay2.addClass('overlay2').css('opacity', 1);
+        
+    }, function(){
+        delet.addClass('yes').css('opacity', "100%");
+        overlay2.addClass('overlay2').css('opacity', -1);
+    });
+
+    $('.card3').hover(function(){
+        delet.addClass('yes').css('opacity', 0);
+        delet.addClass('yes').css('transition', "0.5s");
+        overlay3.addClass('overlay3').css('opacity', 1);
+        
+    }, function(){
+        delet.addClass('yes').css('opacity', "100%");
+        overlay3.addClass('overlay3').css('opacity', -1);
+    });
+
+    $('.card4').hover(function(){
+        delet.addClass('yes').css('opacity', 0);
+        delet.addClass('yes').css('transition', "0.5s");
+        overlay4.addClass('overlay4').css('opacity', 1);
+        
+    }, function(){
+        delet.addClass('yes').css('opacity', "100%");
+        overlay4.addClass('overlay4').css('opacity', -1);
+    });
+
 });
+
