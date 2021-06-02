3ba984e7d4dfc98a14d33ea996a7bb7dec7e6f16
CharlesKristov <charles.christopher@binus.ac.id>
2021-06-02 16:05:05 +0700

    added array maps

--- jsMaps.js
+++ jsMaps.js
@@ -1,6 +1,5 @@
 window.onload = function() {
 
-    // Get the modal
     
     var modal = document.getElementById("ZoomImage");
     
@@ -71,4 +70,103 @@ window.onload = function() {
     span[3].onclick = function() { 
       modal4.style.display = "none";
     }
+
+   
+
+
+
+    var arrmaps=[
+      {
+        'mapname': 'ASCENT',
+        'mapBP': 'ASCENT BLUEPRINT',
+        'mapMG': 'ASCENT MAP GALLERY',
+        'mapdef' : 'An open playground for small wars of position and attrition divide two sites on Ascent. Each site can be fortified by irreversible bomb doors; once they’re down, you’ll have to destroy them or find another way. Yield as little territory as possible.',
+        'MG1' : 'MID ENTRANCE',
+        'MG2' : 'B BOMBSITE',
+        'imgdef' : 'assets/maps/valo1.jpg',
+        'imgBP' : '',
+        'imgMG1' : '',
+        'imgMG2' : ''
+      },
+      {
+        'mapname': 'SPLIT',
+        'mapBP': 'SPLIT BLUEPRINT',
+        'mapMG': 'SPLIT MAP GALLERY',
+        'mapdef' : 'If you want to go far, you’ll have to go up. A pair of sites split by an elevated center allows for rapid movement using two rope ascenders. Each site is built with a looming tower vital for control. Remember to watch above before it all blows sky-high.',
+        'MG1' : '',
+        'MG2' : '',
+        'imgdef' : '',
+        'imgBP' : '',
+        'imgMG1' : '',
+        'imgMG2' : ''
+      },
+      {
+        'mapname': 'HAVEN',
+        'mapBP': 'HAVEN BLUEPRINT',
+        'mapMG': 'HAVEN MAP GALLERY',
+        'mapdef' : "Beneath a forgotten monastery, a clamour emerges from rival Agents clashing to control three sites. There’s more territory to control, but defenders can use the extra real estate for aggressive pushes.",
+        'MG1' : '',
+        'MG2' : '',
+        'imgdef' : '',
+        'imgBP' : '',
+        'imgMG1' : '',
+        'imgMG2' : ''
+      },
+      {
+        'mapname': 'ICEBOX',
+        'mapBP': 'ICEBOX BLUEPRINT',
+        'mapMG': 'ICEBOX MAP GALLERY',
+        'mapdef' : '',
+        'MG1' : '',
+        'MG2' : '',
+        'imgdef' : "Your next battleground is a secret Kingdom excavation site overtaken by the arctic. The two plant sites protected by snow and metal require some horizontal finesse. Take advantage of the ziplines and they’ll never see you coming.",
+        'imgBP' : '',
+        'imgMG1' : '',
+        'imgMG2' : ''
+      },
+      {
+        'mapname': 'BREEZE',
+        'mapBP': 'BREEZE BLUEPRINT',
+        'mapMG': 'BREEZE MAP GALLERY',
+        'mapdef' : "Take in the sights of historic ruins or seaside caves on this tropical paradise. But bring some cover. You'll need them for the wide open spaces and long range engagements. Watch your flanks and this will be a Breeze.",
+        'MG1' : '',
+        'MG2' : '',
+        'imgdef' : '',
+        'imgBP' : '',
+        'imgMG1' : '',
+        'imgMG2' : ''
+      },
+      {
+        'mapname': 'BIND',
+        'mapBP': 'BIND BLUEPRINT',
+        'mapMG': 'BIND MAP GALLERY',
+        'mapdef' : "Two sites. No middle. Gotta pick left or right. What’s it going to be then? Both offer direct paths for attackers and a pair of one-way teleporters make it easier to flank.",
+        'MG1' : '',
+        'MG2' : '',
+        'imgdef' : '',
+        'imgBP' : '',
+        'imgMG1' : '',
+        'imgMG2' : ''
+      },
+    
+    ];
+    
+    document.getElementById("mapname").innerHTML = arrmaps[0].mapname;
+    document.getElementById("teks").innerHTML = arrmaps[0].mapname;
+    document.getElementById("teks2").innerHTML = arrmaps[0].mapname;
+    document.getElementById("teks3").innerHTML = arrmaps[0].mapname;
+    document.getElementById("teks4").innerHTML = arrmaps[0].mapname;
+    document.getElementById("mapdef").innerHTML = arrmaps[0].mapdef;
+    document.getElementById("mapBP").innerHTML = arrmaps[0].mapBP;
+    document.getElementById("mapMG").innerHTML = arrmaps[0].mapMG;
+    document.getElementById("MG1").innerHTML = arrmaps[0].MG1;
+    document.getElementById("MG2").innerHTML = arrmaps[0].MG2;
+    
+
+
+
+
+
+
 };
+
