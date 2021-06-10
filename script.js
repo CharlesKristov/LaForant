96cf9bb144b68539a2e04420e6609489f59c6519
fathariq Dimas <muhammad.octaviandra@binus.ac.id>
2021-06-11 01:38:28 +0700

    kelar sysreq responsive

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
 $(function(){
     const aCaret = Array.from(document.querySelectorAll('.a-caret')), caretSidebar = Array.from(document.querySelectorAll('.caret-sidebar')), sidebarDrop = Array.from(document.querySelectorAll('.sidebar-dropdown')), sidebar = document.querySelector('.sidebar-responsive'), burgerRes = document.querySelector('.burger-button'), closeButton = document.querySelector('.sidebar-close');
 
-let a = null, 
+    let a = null, 
 
-arr = [
-    {
-        'title': 'LAFORANT NEW MAP: BREEZE',
-        'date': '23/04/21',
-        'url': 'https://www.youtube.com/watch?v=HuI3qdaL-jI&ab_channel=VALORANT'
-    },
-    {
-        'title': 'NEW AGENTS REVEAL "ASTRA"',
-        'date': '09/03/21',
-        'url': 'https://playvalorant.com/en-us/news/dev/astra-from-sketch-to-key-art/'
-    }, 
-    {
-        'title': 'LAFORANT NEW MODE: ESCALATION',
-        'date': '16/02/21',
-        'url': 'https://playvalorant.com/en-us/news/game-updates/new-valorant-mode-escalation/'
+    arr = [
+        {
+            'title': 'LAFORANT NEW MAP: BREEZE',
+            'date': '23/04/21',
+            'url': 'https://www.youtube.com/watch?v=HuI3qdaL-jI&ab_channel=VALORANT'
+        },
+        {
+            'title': 'NEW AGENTS REVEAL "ASTRA"',
+            'date': '09/03/21',
+            'url': 'https://playvalorant.com/en-us/news/dev/astra-from-sketch-to-key-art/'
+        }, 
+        {
+            'title': 'LAFORANT NEW MODE: ESCALATION',
+            'date': '16/02/21',
+            'url': 'https://playvalorant.com/en-us/news/game-updates/new-valorant-mode-escalation/'
+        }
+    ], 
+
+    detailNews = [
+        {
+            'title': 'COMMUNITY SPOTLIGHT #7',
+            'date': '14/10/2020',
+            'url': 'https://playvalorant.com/en-us/news/announcements/community-spotlight-7/'
+        },
+        {
+            'title': 'NEW AGENTS REVEAL "SKYE"',
+            'date': '08/10/2020',
+            'url': 'https://www.youtube.com/watch?v=C3QTyMXi-WE&ab_channel=VALORANT'
+        },
+        {
+            'title': 'LAFORANT NEW MAP: ICEBOX',
+            'date': '06/10/2020',
+            'url': 'https://www.youtube.com/watch?v=EB4vFzY3MWc&ab_channel=VALORANT'
+        },
+        {
+            'title': 'COMMUNITY SPOTLIGHT #6',
+            'date': '23/09/2020',
+            'url': 'https://playvalorant.com/en-us/news/announcements/community-spotlight-6/'
+        },
+        {
+            'title': 'COMMUNITY SPOTLIGHT #5',
+            'date': '11/09/2020',
+            'url': 'https://playvalorant.com/en-us/news/announcements/community-spotlight-5/'
+        },
+
+    ], autoSlide = setInterval(autoChange, 3000);
+
+    sliderDetail();
+
+    function sliderDetail(){
+        detailNews.forEach( e => {
+            $('.board-text-more').append($(`<a href="${e.url}" target="_blank">${e.title}<span>${e.date}</span></a>`)) 
+        });
     }
-], 
-
-detailNews = [
-    {
-        'title': 'COMMUNITY SPOTLIGHT #7',
-        'date': '14/10/2020',
-        'url': 'https://playvalorant.com/en-us/news/announcements/community-spotlight-7/'
-    },
-    {
-        'title': 'NEW AGENTS REVEAL "SKYE"',
-        'date': '08/10/2020',
-        'url': 'https://www.youtube.com/watch?v=C3QTyMXi-WE&ab_channel=VALORANT'
-    },
-    {
-        'title': 'LAFORANT NEW MAP: ICEBOX',
-        'date': '06/10/2020',
-        'url': 'https://www.youtube.com/watch?v=EB4vFzY3MWc&ab_channel=VALORANT'
-    },
-    {
-        'title': 'COMMUNITY SPOTLIGHT #6',
-        'date': '23/09/2020',
-        'url': 'https://playvalorant.com/en-us/news/announcements/community-spotlight-6/'
-    },
-    {
-        'title': 'COMMUNITY SPOTLIGHT #5',
-        'date': '11/09/2020',
-        'url': 'https://playvalorant.com/en-us/news/announcements/community-spotlight-5/'
-    },
-
-];
-
-sliderDetail();
-
-function sliderDetail(){
-    detailNews.forEach( e => {
-        $('.board-text-more').append($(`<a href="${e.url}" target="_blank">${e.title}<span>${e.date}</span></a>`)) 
-    });
-}
 
     const slidesImage = $('.slide-image'), counts = $('.count');
 
@@ -69,30 +69,44 @@ function sliderDetail(){
     $('.detail-image-date').html(arr[curr].date);
 
     $('#left').click(()=>{
-        slidesImage.eq(curr).hide()
+        let numCurr = curr, numNext;
         if(curr == 0){
             curr = 2;
         }else{
             --curr;
         }
-        $('.detail-image-text').html(arr[curr].title);
-        $('.detail-image-date').html(arr[curr].date);
-        counts.html(curr + 1);
-        slidesImage.eq(curr).fadeIn();
+        numNext = curr;
+        clearInterval(autoSlide);
+        autoSlide = setInterval(autoChange, 3000);
+        changeImg(numCurr, numNext);
     })
 
     $('#right').click(()=>{
-        slidesImage.eq(curr).hide()
+        let numCurr = curr, numNext;
         if(curr == 2){
             curr = 0
         }else{
             ++curr
         }
-        $('.detail-image-text').html(arr[curr].title)
-        $('.detail-image-date').html(arr[curr].date)
-        counts.html(curr + 1)
-        slidesImage.eq(curr).fadeIn()
+        numNext = curr;
+        clearInterval(autoSlide);
+        autoSlide = setInterval(autoChange, 3000);
+        changeImg(numCurr, numNext);
     })
+    
+    function autoChange(){
+        let numCurr = curr;
+        curr = curr + 1 <= 2 ? curr + 1 : 0;
+        changeImg(numCurr, curr);
+    }
+
+    function changeImg(numCurr, numNext){
+        slidesImage.eq(numCurr).hide()
+        $('.detail-image-text').html(arr[numNext].title)
+        $('.detail-image-date').html(arr[numNext].date)
+        counts.html(numNext + 1)
+        slidesImage.eq(numNext).fadeIn()
+    }
 
     burgerRes.addEventListener('click', () =>{
         sidebar.style.display = 'flex';
